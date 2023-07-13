9f5978f08ba9102613914f5b64a7d0537e3c64cf
Jordan Pittman <jordan@cryptica.me>
2023-07-12 20:22:21 -0400

    Sort classes
    CLI: `npx prettier -w src`

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="items-center flex-col flex p-24 min-h-screen justify-between">
-      <div className="w-full text-sm lg:flex max-w-5xl z-10 items-center font-mono justify-between">
-        <p className="dark:from-inherit border-b fixed dark:border-neutral-800 lg:border lg:bg-gray-200 lg:p-4 top-0 justify-center left-0 w-full bg-gradient-to-b lg:dark:bg-zinc-800/30 pt-8 lg:static lg:w-auto backdrop-blur-2xl from-zinc-200 pb-6 border-gray-300 flex lg:rounded-xl dark:bg-zinc-800/30">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          <code className="font-bold font-mono">src/app/page.js</code>
+          <code className="font-mono font-bold">src/app/page.js</code>
         </p>
-        <div className="dark:via-black lg:h-auto items-end bg-gradient-to-t lg:bg-none fixed lg:static left-0 from-white h-48 flex via-white justify-center lg:w-auto w-full dark:from-black bottom-0">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="lg:pointer-events-auto place-items-center p-8 pointer-events-none lg:p-0 flex gap-2"
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -28,9 +28,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="after:translate-x-1/3 after:absolute after:h-[180px] place-items-center before:rounded-full before:bg-gradient-radial before:dark:opacity-10 after:content-[''] before:absolute after:from-sky-200 before:h-[300px] before:lg:h-[360px] after:blur-2xl after:via-blue-200 before:dark:to-blue-700 before:from-white after:dark:via-[#0141ff] before:blur-2xl before:content-[''] before:-translate-x-1/2 after:w-[240px] before:dark:from-transparent flex after:bg-gradient-conic after:-z-20 before:w-[480px] after:dark:from-sky-900 after:dark:opacity-40 before:dark:bg-gradient-to-br relative before:to-transparent">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
-          className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert relative"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
@@ -39,16 +39,16 @@ export default function Home() {
         />
       </div>
 
-      <div className="lg:text-left text-center lg:grid-cols-4 grid mb-32 lg:mb-0">
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="hover:border-gray-300 hover:dark:bg-neutral-800/30 px-5 rounded-lg transition-colors border group border-transparent hover:dark:border-neutral-700 py-4 hover:bg-gray-100"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{" "}
-            <span className="group-hover:translate-x-1 motion-reduce:transform-none transition-transform inline-block">
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
@@ -59,13 +59,13 @@ export default function Home() {
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="hover:bg-gray-100 hover:dark:bg-neutral-800 transition-colors group hover:dark:bg-opacity-30 hover:border-gray-300 border-transparent px-5 rounded-lg border py-4 hover:dark:border-neutral-700"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Learn{" "}
-            <span className="motion-reduce:transform-none inline-block transition-transform group-hover:translate-x-1">
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
@@ -76,13 +76,13 @@ export default function Home() {
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="border-transparent hover:bg-gray-100 hover:dark:border-neutral-700 rounded-lg group px-5 border py-4 hover:dark:bg-neutral-800/30 hover:border-gray-300 transition-colors"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{" "}
-            <span className="group-hover:translate-x-1 inline-block motion-reduce:transform-none transition-transform">
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
@@ -93,13 +93,13 @@ export default function Home() {
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="hover:dark:bg-neutral-800/30 border-transparent py-4 hover:border-gray-300 transition-colors rounded-lg hover:dark:border-neutral-700 group border hover:bg-gray-100 px-5"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{" "}
-            <span className="motion-reduce:transform-none inline-block transition-transform group-hover:translate-x-1">
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
